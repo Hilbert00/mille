a914c9a990659d65ca3289e2b709ec51737c96b7
Hilbert <nicolashilbert000@gmail.com>
2023-01-15 23:50:59 -0300

    :recycle: refactor: Refatoração rota usuário

--- backend/src/Routes/User.route.ts
+++ backend/src/Routes/User.route.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import conn from "../Config/Database.config.js";
 
 // MIDDLEWARES
 import { verifyToken } from "../Middlewares/Auth.middleware.js";
@@ -7,22 +6,9 @@ import { verifyToken } from "../Middlewares/Auth.middleware.js";
 const router = Router();
 
 router.get("/@:user", verifyToken, (req, res) => {
-    const userName = req.params.user;
+    const user = req.user;
 
-    if (userName === req.params.user) {
-        const query = "SELECT username, challenge_matches, challenge_wins, user_level FROM users WHERE ?? = ?";
-        const data = ["username", userName];
-
-        conn.query(query, data, (err, result) => {
-            if (err) {
-                console.log(err);
-            }
-
-            return res.json({ ...result[0] });
-        });
-    } else {
-        return res.status(401).json({ message: "nope" });
-    }
+    return res.json(user);
 });
 
 export default router;
