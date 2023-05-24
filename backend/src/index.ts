3091eeb609a6114cd4ba5d2a684866831558108e
Hilbert <nicolashilbert000@gmail.com>
2023-05-24 00:37:07 -0300

    Revert ":white_check_mark: test: CORS all origins"
    This reverts commit 0aefaf2ca3cc5a30e12ca57b6a43228fa486a4cf.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: "*",
+        origin: "http://localhost:3000",
         credentials: true,
         preflightContinue: true,
     })
