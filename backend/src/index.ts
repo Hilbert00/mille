0aefaf2ca3cc5a30e12ca57b6a43228fa486a4cf
Hilbert <nicolashilbert000@gmail.com>
2023-05-24 00:17:34 -0300

    :white_check_mark: test: CORS all origins

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "*",
         credentials: true,
         preflightContinue: true,
     })
