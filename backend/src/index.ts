06c5710f23db1d35ad1a6e4a5839dd668e41ef0a
Hilbert <nicolashilbert000@gmail.com>
2023-05-28 02:42:47 -0300

    :white_check_mark: test: Host API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: process.env.FRONTEND_URL,
         credentials: true,
         preflightContinue: true,
     })
