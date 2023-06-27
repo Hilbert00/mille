38319cadc34490360cfa08779426366ec1bd59e5
Hilbert <nicolashilbert000@gmail.com>
2023-06-27 01:39:18 -0300

    :bug: fix: Keep database connection alive

--- backend/src/Config/Database.config.ts
+++ backend/src/Config/Database.config.ts
@@ -11,4 +11,10 @@ const conn = mysql.createConnection({
     port: 3306,
 });
 
+setInterval(() => {
+    conn.query("SELECT 1", (err, _res) => {
+        if (err) console.log(err);
+    });
+}, 5000);
+
 export default conn;
