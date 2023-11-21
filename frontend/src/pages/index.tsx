c79c33dd9f99c721d246249ab1b76fef322eaf7f
Hilbert <nicolashilbert000@gmail.com>
2023-11-21 11:00:16 -0300

    :lipstick: feat: Title centering on desktop

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -13,13 +13,13 @@ export default function LandingPage() {
                 <title>Mille - Plataforma de Estudos para o ENEM</title>
             </Head>
 
-            <header className="flex h-28 w-full items-center justify-center">
+            <header className="flex h-28 w-full items-center justify-center sm:absolute">
                 <Logo type={"full"} />
             </header>
 
             <div className="w-full overflow-hidden">
                 <main className="mx-auto min-h-[calc(100vh-9rem)] max-w-[calc(100vw-40px)] md:max-w-3xl">
-                    <section className="mb-6 sm:mb-0 sm:flex sm:min-h-[calc(100vh-7rem)] sm:flex-col sm:justify-center">
+                    <section className="mb-6 sm:mb-0 sm:flex sm:min-h-screen sm:flex-col sm:justify-center">
                         <div className="mb-6 sm:mb-14">
                             <h1 className="mb-3 text-center text-2xl font-bold sm:mb-8 sm:text-7xl">
                                 Venha hoje estudar com a gente!
@@ -38,7 +38,7 @@ export default function LandingPage() {
                     </section>
 
                     <section className="relative flex flex-col justify-between gap-6">
-                        <div className="absolute -left-full -z-10 w-[200vw] bg-blue-600 h-full"></div>
+                        <div className="absolute -left-full -z-10 h-full w-[200vw] bg-blue-600"></div>
                         <FeatureDisplay title={"2 mundos únicos!"} type={"solo"}>
                             Dois mundos disponíveis para as seguintes áreas do conhecimento: Matemática e Ciências da
                             Natureza.
